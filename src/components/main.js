8ac249354283e822b1f4c898cbb4ccf8a9181ccf
Colin Elsinga <colinelsinga@Colins-MBP-2.router.taotronics.com>
2021-03-08 19:17:35 -0800

    add date greeting feature

--- src/components/main.js
+++ src/components/main.js
@@ -2,15 +2,37 @@ import { Component } from "react";
 import './main.scss';
 
 export default function Main() {
+
+  let myDate = new Date();
+  let hrs = myDate.getHours();
+  let greet;
+  if (hrs < 12) {
+    greet = 'Good Morning';
+  } else if (hrs >= 12 && hrs <= 17) {
+    greet = 'Good Afternoon';
+  } else if (hrs >= 17 && hrs <= 24) {
+    greet = 'Good Evening';
+  }
+
   return (
     <div className="main" style={{display: 'flex', flexDirection: 'column'}}>
         <div className="name">
-          Colin Elsinga
+          {greet}!
         </div>
         <div className="subtitle">
-          Full Stack Developer
+          I am a passionate full stack developer based in Vancouver, Canada. 
         </div>
-        <p className="location">📍 Vancouver, BC</p>
+        <div className="skills">
+          <div className="name">
+          My Skills
+          </div>
+          <p>JavaScript/ES6 • React • Node/Express • PostgreSQL • JQuery • Ruby/Rails • AWS • Python</p>
+          <div className="name">
+            Projects
+          </div>
+          <p>Project 1 Project 2 Project 3</p>
+        </div>
+
       </div>
   )
 }
