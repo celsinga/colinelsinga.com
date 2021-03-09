8891744cbbca4d3a4cd8e59b9e4f6d2ca0ab6b01
Colin Elsinga <colinelsinga@gmail.com>
2021-03-08 23:17:32 -0800

    add photos for projects

--- src/components/main.js
+++ src/components/main.js
@@ -2,11 +2,17 @@ import { Component } from "react";
 import './main.scss';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import nba from './nba.png';
+import scheduler from './app2.png';
+import blockchain from './blockchain.png';
+import altru from './altru.png';
+import kigogo from './homepage.png'
+import tweeter from './tweeter.png'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     minWidth: '24em',
-    minHeight: '14em',
+    minHeight: '16em',
     margin: `${theme.spacing(1)}px ${theme.spacing(1)}px`,
     padding: theme.spacing(2),
     backgroundColor: 'transparent',
@@ -50,27 +56,33 @@ export default function Main() {
           My Skills
           </div>
           <p>JavaScript/ES6 • React • Node/Express • PostgreSQL • HTML5/CSS • JQuery • Ruby/Rails • AWS • Python</p>
-          <div className="name">
+          <div className="skills">
             Projects
           </div>
           <div className="projects">
             <Paper className={classes.paper}>
-              NBA Dashboard
+              <p>NBA Dashboard</p>
+              <img src={nba}></img>
             </Paper>
             <Paper className={classes.paper}>
-              Scheduler
+              <p>Scheduler</p>
+              <img src={scheduler}></img>
             </Paper>
             <Paper className={classes.paper}>
-              altru.link
+              <p>altru.link</p>
+              <img src={altru}></img>
             </Paper>
             <Paper className={classes.paper}>
-              JS Blockchain
+              <p>JS Blockchain</p>
+              <img src={blockchain}></img>
             </Paper>
             <Paper className={classes.paper}>
-              Tweeter
+              <p>Tweeter</p>
+              <img src={tweeter}></img>
             </Paper>
             <Paper className={classes.paper}>
-              Kigogo
+              <p>Kigogo</p>
+              <img src={kigogo}></img>
             </Paper>
           </div>
         </div>
